9b61c0aa2b36f6e24ea3f5cc77f93f602980c55c
Artur Borisovich <thesunwave@yandex.ru>
2016-12-05 21:55:21 +0300

    Add spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,9 +42,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  DatabaseCleaner.strategy = :transaction
-
-  config.before(:all) do
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean
     Factory.reset
   end
