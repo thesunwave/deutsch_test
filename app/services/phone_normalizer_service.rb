7ef30491d1cf159705c423315fa001e0b5177fc1
Artur Borisovich <thesunwave@yandex.ru>
2016-12-07 02:02:40 +0300

    Improving phone normalizer

--- app/services/phone_normalizer_service.rb
+++ app/services/phone_normalizer_service.rb
@@ -2,15 +2,32 @@ class PhoneNormalizerService
   def call(args)
     arr = args.split(' ')
     arr.map do |elem|
-      begin
-        if Numberplan.find_by_prefix(elem).present?
-          elem
+        next prefix_by_phony(elem) if Phony.plausible?(elem)
+        trimmed_str = strip_zero_by_start_string(elem)
+        objs = Numberplan.where(prefix: scan_prefix(trimmed_str)).pluck(:prefix)
+
+        if objs.present?
+          next trimmed_str if objs.include?(0)
+          objs.join(' ')
         else
-          Phony.format(Phony.normalize(elem), format: :international).split[1]
+          trimmed_str
         end
-      rescue
-        elem
-      end
+
     end.join(' ')
+  end
+
+  private
+
+  def scan_prefix(elem)
+    return elem if elem.length < 3
+    [elem, *scan_prefix(elem[0..-2])]
+  end
+
+  def prefix_by_phony(elem)
+    Phony.format(Phony.normalize(elem), format: :international).split[1]
+  end
+
+  def strip_zero_by_start_string(elem)
+    elem.gsub(/^0/, '')
   end
 end
